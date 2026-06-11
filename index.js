b04f37eab7d31c5ed1aca0d85a21061bed9a2e49
silvia <silviakoltun@gmail.com>
2026-06-10 22:09:10 -0300

    Apaguei a rota default (/) padrão de servidor funcionando.

--- index.js
+++ index.js
@@ -2,29 +2,26 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-    res.json({ mensagem: 'Servidor funcionando! 🎉' })
-  })
   
-  app.get('/',(req, res) =>{
-    res.json({
-      projeto: 'Loja Virtual',
-        descricao: 'Site para vendas de produtos pela internet',
-        status: 'online'
-      })
+app.get('/',(req, res) =>{
+  res.json({
+    projeto: 'Loja Virtual',
+      descricao: 'Site para vendas de produtos pela internet',
+      status: 'online'
+    })
 })
-  app.get('/produtos', (req, res) => {
+
+app.get('/produtos', (req, res) => {
   res.json([
     { id: 1, nome: 'Notebook Gamer', preco: 4500.00, categoria: 'Informática' },
     { id: 2, nome: 'Cadeira Ergonômica', preco: 1200.00, categoria: 'Móveis' },
     { id: 3, nome: 'Mouse sem fio', preco: 150.00, categoria: 'Acessórios' }
   ])
-
 })
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`)
-  })
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
 
 
 
