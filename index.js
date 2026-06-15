6cccbea937c2158f63a5c1bd5b7a4f545a44f60a
Silvia Borges <silviakoltun@gmail.com>
2026-06-15 20:04:42 +0000

    'E03 com GET, Post, Postman'

--- index.js
+++ index.js
@@ -3,22 +3,69 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-  
-app.get('/',(req, res) =>{
-  res.json({
-    projeto: 'Loja Virtual',
-      descricao: 'Site para vendas de produtos pela internet',
-      status: 'online'
-    })
-})
+app.use(express.json());
 
+const produtos = [
+  {
+    id: 1,
+    nome: 'Suporte para Celular 3D',
+    preco: 25.90,
+    categoria: 'acessorios',
+    estoque: 10
+  },
+  {
+    id: 2,
+    nome: 'Organizador de Cabos 3D',
+    preco: 15.50,
+    categoria: 'organizadores',
+    estoque: 20
+  },
+  {
+    id: 3,
+    nome: 'Vaso Decorativo 3D',
+    preco: 39.90,
+    categoria: 'decoração',
+    estoque: 5
+  }
+];
+
+// GET - Listar todos os produtos
 app.get('/produtos', (req, res) => {
-  res.json([
-    { id: 1, nome: 'Notebook Gamer', preco: 4500.00, categoria: 'Informática' },
-    { id: 2, nome: 'Cadeira Ergonômica', preco: 1200.00, categoria: 'Móveis' },
-    { id: 3, nome: 'Mouse sem fio', preco: 150.00, categoria: 'Acessórios' }
-  ])
-})
+  res.json(produtos);
+});
+
+// POST - Adicionar um novo produto
+app.post('/produtos', (req, res) => {
+  const { nome, preco, estoque } = req.body;
+
+  const novoProduto = {
+    id: produtos.length + 1,
+    nome,
+    preco,
+    estoque
+  };
+
+  produtos.push(novoProduto);
+
+  res.status(201).json(novoProduto);
+});
+
+app.post('mensagem', (req, res) =>{
+  const {categoria, estoque} = req, body;
+  if (!categoria || !estoque){
+    return res.status(400).json({
+      erro: 'categoria e estoque são obrigatórios'
+    })
+  }
+  const novaMensagem = {
+    id: mensagem.length +1,
+    categoria,
+    estoque
+  };
+  mensagem.push(novaMensagem);
+  res.status(201).json(novaMensagem)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
---
+++ livros.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+const livros = [
+  { id: 1, titulo: 'O Alquimista',  genero: 'Romance',          disponivel: true  },
+  { id: 2, titulo: '1984',          genero: 'Ficção Científica', disponivel: false },
+  { id: 3, titulo: 'Dom Casmurro',  genero: 'Romance',          disponivel: true  },
+  { id: 4, titulo: 'Sapiens',       genero: 'Não-Ficção',       disponivel: true  }
+]
+app.get('/livros', (req, res) => {
+  const { genero } = req.query
+
+  // Se vier o filtro de gênero, filtra o array
+  if (genero) {
+    const filtrados = livros.filter(livro => livro.genero === genero)
+    return res.json(filtrados)
+  }
+
+  // Sem filtro, retorna todos
+  res.json(livros)
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
